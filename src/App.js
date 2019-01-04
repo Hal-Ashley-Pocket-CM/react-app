66392c43d8167db6304b07f90af2c3917ce9ff00
Ashley <anoelj86@yahoo.com>
2019-01-04 12:39:20 -0700

    html router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import { Switch, Route } from 'react-router';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/dash" component={Dashboard} />
+          {/* <Route exact path="/saved/notes" component={Saved} /> */}
+          {/* <Route component={NoMatch} /> */}
+        </Switch>
+      </div>
+    );
   }
 }
 
