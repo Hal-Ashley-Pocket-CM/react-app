fe12a5ab54d3aa75cb4841c587feb4d9e0eaac28
Ashley <anoelj86@yahoo.com>
2019-01-09 21:23:05 -0700

    navbar easy routes for building

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,16 @@ const Nav = props => (
   <div>
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
-        {/* <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""> */}
+        {/* <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="Pocket Case Manager"> */}
+      </a>
+      <a className="navbar-brand" href="dash">
+        Dashboard
+      </a>
+      <a className="navbar-brand" href="/connect">
+        Connect
+      </a>
+      <a className="navbar-brand" href="/login">
+        Logout
       </a>
     </nav>
   </div>
